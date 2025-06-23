RossPorter506/msp430fr2x5x-hal-eh1.0

Add backup memory

src/lib.rs
@@ -62,6 +62,7 @@ pub mod i2c;
 pub mod spi;
 pub mod sac;
 pub mod ecomp;
+pub mod backup_mem;
 
 mod hw_traits;
 mod util;

YuhanLiin/msp430fr2x5x-hal

src/lib.rs
@@ -62,6 +62,7 @@ pub mod i2c;
 pub mod spi;
 pub mod sac;
 pub mod ecomp;
+pub mod bak_mem;
 
 mod hw_traits;
 mod util;