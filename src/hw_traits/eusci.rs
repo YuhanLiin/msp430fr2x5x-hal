RossPorter506/msp430fr2x5x-hal-eh1.0

Reduce I2C boilerplate
Use default() to massively reduce boilerplate,
Use existing type from hw traits, saves reinventing the wheel
Remove unused regs

src/hw_traits/eusci.rs
@@ -61,12 +61,17 @@ pub enum Ucmode {
     I2CMode = 3,
 }
 
+/// Configure the automatic glitch filter on the SDA and SCL lines
 #[derive(Copy, Clone, Default)]
 pub enum Ucglit {
+    /// Pulses of maximum 50-ns length are filtered.
     #[default]
     Max50ns = 0,
+    /// Pulses of maximum 25-ns length are filtered.
     Max25ns = 1,
+    /// Pulses of maximum 12.5-ns length are filtered.
     Max12_5ns = 2,
+    /// Pulses of maximum 6.25-ns length are filtered.
     Max6_25ns = 3,
 }
 
@@ -149,54 +154,11 @@ pub struct UcbCtlw1, UcbCtlw1_rd, UcbCtlw1_wr {
 }
 
 // in order to avoid 4 separate structs, I manually implemented the macro for these registers
+#[derive(Debug, Default)]
 pub struct UcbI2coa {
     pub ucgcen: bool,
     pub ucoaen: bool,
     pub i2coa0: u16,
-}
-
-reg_struct! {
-pub struct UcbIe, UcbIe_rd, UcbIe_wr {
-    flags{
-        pub ucbit9ie: bool,
-        pub uctxie3: bool,
-        pub ucrxie3: bool,
-        pub uctxie2: bool,
-        pub ucrxie2: bool,
-        pub uctxie1: bool,
-        pub ucrxie1: bool,
-        pub uccltoie: bool,
-        pub ucbcntie: bool,
-        pub ucnackie: bool,
-        pub ucalie: bool,
-        pub ucstpie: bool,
-        pub ucsttie: bool,
-        pub uctxie0: bool,
-        pub ucrxie0: bool,
-    }
-}
-}
-
-reg_struct! {
-pub struct UcbIFG, UcbIFG_rd, UcbIFG_wr{
-    flags{
-        pub ucbit9ifg: bool,
-        pub uctxifg3: bool,
-        pub ucrxifg3: bool,
-        pub uctxifg2: bool,
-        pub ucrxifg2: bool,
-        pub uctxifg1: bool,
-        pub ucrxifg1: bool,
-        pub uccltoifg: bool,
-        pub ucbcntifg: bool,
-        pub ucnackifg: bool,
-        pub ucalifg: bool,
-        pub ucstpifg: bool,
-        pub ucsttifg: bool,
-        pub uctxifg0: bool,
-        pub ucrxifg0: bool,
-    }
-}
 }
 
 reg_struct! {
@@ -305,10 +267,10 @@ pub trait EUsciI2C: Steal {
     fn i2csa_rd(&self) -> u16;
     fn i2csa_wr(&self, val: u16);
 
-    fn ie_wr(&self, reg: &UcbIe);
+    fn ie_wr(&self, reg: u16);
 
     fn ifg_rd(&self) -> Self::IfgOut;
-    fn ifg_wr(&self, reg: &UcbIFG);
+    fn ifg_wr(&self, reg: u16);
     fn ifg_rst(&self);
 
     fn iv_rd(&self) -> u16;
@@ -917,8 +879,8 @@ macro_rules! eusci_b_impl {
             }
 
             #[inline(always)]
-            fn ie_wr(&self, reg: &UcbIe) {
-                self.$ucbxie().write(UcbIe_wr! {reg});
+            fn ie_wr(&self, reg: u16) {
+                self.$ucbxie().write(|w| unsafe{w.bits(reg)} );
             }
 
             #[inline(always)]
@@ -927,8 +889,8 @@ macro_rules! eusci_b_impl {
             }
 
             #[inline(always)]
-            fn ifg_wr(&self, reg: &UcbIFG) {
-                self.$ucbxifg().write(UcbIFG_wr! {reg});
+            fn ifg_wr(&self, reg: u16) {
+                self.$ucbxifg().write(|w| unsafe{w.bits(reg)} );
             }
 
             #[inline(always)]

YuhanLiin/msp430fr2x5x-hal

src/hw_traits/eusci.rs
@@ -61,12 +61,17 @@ pub enum Ucmode {
     I2CMode = 3,
 }
 
+/// Configure the automatic glitch filter on the SDA and SCL lines
 #[derive(Copy, Clone, Default)]
 pub enum Ucglit {
+    /// Pulses of maximum 50-ns length are filtered.
     #[default]
     Max50ns = 0,
+    /// Pulses of maximum 25-ns length are filtered.
     Max25ns = 1,
+    /// Pulses of maximum 12.5-ns length are filtered.
     Max12_5ns = 2,
+    /// Pulses of maximum 6.25-ns length are filtered.
     Max6_25ns = 3,
 }
 
@@ -149,54 +154,11 @@ pub struct UcbCtlw1, UcbCtlw1_rd, UcbCtlw1_wr {
 }
 
 // in order to avoid 4 separate structs, I manually implemented the macro for these registers
+#[derive(Debug, Default)]
 pub struct UcbI2coa {
     pub ucgcen: bool,
     pub ucoaen: bool,
     pub i2coa0: u16,
-}
-
-reg_struct! {
-pub struct UcbIe, UcbIe_rd, UcbIe_wr {
-    flags{
-        pub ucbit9ie: bool,
-        pub uctxie3: bool,
-        pub ucrxie3: bool,
-        pub uctxie2: bool,
-        pub ucrxie2: bool,
-        pub uctxie1: bool,
-        pub ucrxie1: bool,
-        pub uccltoie: bool,
-        pub ucbcntie: bool,
-        pub ucnackie: bool,
-        pub ucalie: bool,
-        pub ucstpie: bool,
-        pub ucsttie: bool,
-        pub uctxie0: bool,
-        pub ucrxie0: bool,
-    }
-}
-}
-
-reg_struct! {
-pub struct UcbIFG, UcbIFG_rd, UcbIFG_wr{
-    flags{
-        pub ucbit9ifg: bool,
-        pub uctxifg3: bool,
-        pub ucrxifg3: bool,
-        pub uctxifg2: bool,
-        pub ucrxifg2: bool,
-        pub uctxifg1: bool,
-        pub ucrxifg1: bool,
-        pub uccltoifg: bool,
-        pub ucbcntifg: bool,
-        pub ucnackifg: bool,
-        pub ucalifg: bool,
-        pub ucstpifg: bool,
-        pub ucsttifg: bool,
-        pub uctxifg0: bool,
-        pub ucrxifg0: bool,
-    }
-}
 }
 
 reg_struct! {
@@ -305,10 +267,10 @@ pub trait EUsciI2C: Steal {
     fn i2csa_rd(&self) -> u16;
     fn i2csa_wr(&self, val: u16);
 
-    fn ie_wr(&self, reg: &UcbIe);
+    fn ie_wr(&self, reg: u16);
 
     fn ifg_rd(&self) -> Self::IfgOut;
-    fn ifg_wr(&self, reg: &UcbIFG);
+    fn ifg_wr(&self, reg: u16);
     fn ifg_rst(&self);
 
     fn iv_rd(&self) -> u16;
@@ -917,8 +879,8 @@ macro_rules! eusci_b_impl {
             }
 
             #[inline(always)]
-            fn ie_wr(&self, reg: &UcbIe) {
-                self.$ucbxie().write(UcbIe_wr! {reg});
+            fn ie_wr(&self, reg: u16) {
+                self.$ucbxie().write(|w| unsafe { w.bits(reg) });
             }
 
             #[inline(always)]
@@ -927,8 +889,8 @@ macro_rules! eusci_b_impl {
             }
 
             #[inline(always)]
-            fn ifg_wr(&self, reg: &UcbIFG) {
-                self.$ucbxifg().write(UcbIFG_wr! {reg});
+            fn ifg_wr(&self, reg: u16) {
+                self.$ucbxifg().write(|w| unsafe { w.bits(reg) });
             }
 
             #[inline(always)]