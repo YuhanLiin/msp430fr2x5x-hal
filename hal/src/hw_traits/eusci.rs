67e5d875166e99a0224be87994fb883114858c25
Enmanuel Parache <enmanuelparache@gmail.com>
2026-08-10 15:25:40 -0400

    fix: use msp430fr25x2 version with ucbusy bit and password enum

--- hal/src/hw_traits/eusci.rs
+++ hal/src/hw_traits/eusci.rs
@@ -488,7 +488,7 @@ macro_rules! eusci_spi_impl {
             }
 
             fn is_busy(&self) -> bool {
-                (self.$ucxstatw().read().bits() & 1) != 0
+                self.$ucxstatw().read().ucbusy().bit()
             }
         }
 
@@ -510,7 +510,7 @@ macro_rules! eusci_spi_impl {
 
             #[inline(always)]
             fn ucbusy(&self) -> bool {
-                (self.bits() & 1) != 0
+                self.ucbusy().bit()
             }
         }
     };
--- hal/src/pmm.rs
+++ hal/src/pmm.rs
@@ -53,9 +53,7 @@ impl Pmm {
             true => None,
             false => {
                 // Unlock PMM registers
-                self.0
-                    .pmmctl0()
-                    .modify(|_, w| unsafe { w.pmmpw().bits(0xA5) });
+                self.0.pmmctl0().modify(|_, w| w.pmmpw().password() );
 
                 self.0.pmmctl2().write(|w| unsafe{ w
                     .bits(pmmctl2.bits()) 
